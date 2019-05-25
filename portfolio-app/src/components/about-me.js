038221d42987008e82490d92b56f848b35761425
Sarah Tam <sarahtam718@gmail.com>
2019-05-25 08:36:33 -0500

    satisfied with footer

--- portfolio-app/src/components/about-me.js
+++ portfolio-app/src/components/about-me.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./images/linkedInPic.jpg";
 
 const About = () => (
   <div>
--- portfolio-app/src/components/footer.js
+++ portfolio-app/src/components/footer.js
@@ -2,8 +2,13 @@ import React from "react";
 
 const Footer = () => (
   <div>
-    <p>Copyright Sarah Tam 2019</p>
-    <p> Github +++ LinkedIn </p>
+    <footer className="page-footer font-small bg-light fixed-bottom">
+      <div className="footer-copyright text-center py-3">
+        Copyright Sarah Tam 2019 - Check out her
+        <a href="https://github.com/sarahtam718"> Github</a> +
+        <a href="https://www.linkedin.com/in/sarah-tam"> LinkedIn</a>
+      </div>
+    </footer>
   </div>
 );
 
