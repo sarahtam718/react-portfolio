ba3c4d7d3659bd9a9c1a7ba641bb9392f33269ef
Sarah Tam <sarahtam718@gmail.com>
2019-06-04 18:20:15 -0500

    react create app on heroku

--- portfolio-app/src/components/navbar.js
+++ portfolio-app/src/components/navbar.js
@@ -3,10 +3,9 @@ import SmallLogo from "./images/small-green-dot-logo.png";
 
 const Navbar = () => (
   <nav
-    className="navbar navbar-expand-lg navbar-light bg-light shadow"
+    className="navbar  navbar-light bg-light shadow"
     style={{
       backgroundColor: "white",
-      height: "2.5rem",
       color: "#613a43"
     }}
   >
@@ -24,34 +23,21 @@ const Navbar = () => (
       />{" "}
       Sarah Tam
     </a>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarNavAltMarkup"
-      aria-controls="navbarNavAltMarkup"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon" />
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a
-          class="nav-item nav-link"
-          style={{ color: "#613A43" }}
-          href="https://github.com/sarahtam718"
-        >
-          Github
-        </a>
-        <a
-          class="nav-item nav-link"
-          style={{ color: "#613A43" }}
-          href="https://linkedin.com/in/sarah-tam"
-        >
-          LinkedIn
-        </a>
-      </div>
+    <div class="navbar-nav navbar-expand-lg mr-lg-auto">
+      <a
+        class="nav-item nav-link p-2"
+        style={{ color: "#613A43" }}
+        href="https://github.com/sarahtam718"
+      >
+        Github
+      </a>
+      <a
+        class="nav-item nav-link"
+        style={{ color: "#613A43" }}
+        href="https://linkedin.com/in/sarah-tam"
+      >
+        LinkedIn
+      </a>
     </div>
     <span class="navbar-text" style={{ color: "#613A43" }}>
       full stack web developer
