2a7ff5fa2fd93c14cfb13a50999f817705318967
Sarah Tam <sarahtam718@gmail.com>
2020-01-04 12:14:57 -0600

    styled the navbar - so pretty and mobile-friendly!

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,47 +1,19 @@
-import React from "react";
-import SmallLogo from "./images/small-green-dot-logo.png";
+import React from 'react';
+import SmallLogo from './images/small-green-dot-logo.png';
 
 const Navbar = () => (
-  <nav
-    className="navbar navbar-light bg-light shadow"
-    style={{
-      backgroundColor: "white",
-      color: "#613a43"
-    }}
-  >
-    <a
-      className="navbar-brand"
-      style={{ color: "#613A43" }}
-      href="https://github.com/sarahtam718"
-    >
-      <img
-        src={SmallLogo}
-        width="30"
-        height="30"
-        className="d-inline-block align-top"
-        alt=""
-      />{" "}
-      Sarah Tam
-    </a>
-    <div className="navbar-nav navbar-expand-lg mr-lg-auto">
-      <a
-        className="nav-item nav-link p-2"
-        style={{ color: "#613A43" }}
-        href="https://github.com/sarahtam718"
-      >
-        Github
+  <nav>
+    <img className='logo' src={SmallLogo} alt='' />
+    <div className='nav-links'>
+      <a className='name-link' href='https://github.com/sarahtam718'>
+        Sarah Tam
       </a>
-      <a
-        className="nav-item nav-link"
-        style={{ color: "#613A43" }}
-        href="https://linkedin.com/in/sarah-tam"
-      >
-        LinkedIn
-      </a>
+      <p>full stack web developer</p>
+      <div className='site-links'>
+        <a href='https://github.com/sarahtam718'>Github</a>
+        <a href='https://linkedin.com/in/sarah-tam'>LinkedIn</a>
+      </div>
     </div>
-    <span className="navbar-text" style={{ color: "#613A43" }}>
-      full stack web developer
-    </span>
   </nav>
 );
 
